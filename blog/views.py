a4124ff3a61ef7062b6eb4ef01c7160aba882708
tiken14 <tiken14@live.com>
2025-09-23 20:27:32 +0500

    Небольшие изменения логике создания комментария

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from .models import Article
 from .serializers import ArticleSerializer, CommentarySerializer
 
 
@@ -9,14 +10,14 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class CommentaryViewSet(viewsets.ModelViewSet):
     serializer_class = CommentarySerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        commentary = serializer.save()
-        commentary.author = self.request.user
-        commentary.article = self.kwargs.get("pk")
-        commentary.save()
-
+        article_obj = get_object_or_404(Article, pk=self.kwargs.get("pk"))
+        serializer.save(author=self.request.user, article=article_obj)
