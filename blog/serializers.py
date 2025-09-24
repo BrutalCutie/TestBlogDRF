294fee3cd352d0520d2e45a2cd25f07e3c35273b
tiken14 <tiken14@live.com>
2025-09-24 09:26:17 +0500

    Закрываем правами доступа комментарии и статьи

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,18 +3,19 @@ from rest_framework import serializers
 from .models import Article, Commentary, Category
 
 
+class CommentarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commentary
+        fields = "__all__"
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(),
         allow_null=False
     )
+    commentaries = CommentarySerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
-        fields = "__all__"
-
-
-class CommentarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Commentary
         fields = "__all__"
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Article
+from .permissions import IsOwner
 from .serializers import ArticleSerializer, CommentarySerializer
 
 
@@ -10,8 +11,15 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Article.objects.prefetch_related('commentaries').filter(is_active=True)
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ('destroy', 'update', 'partial_update'):
+            self.permission_classes = [IsAuthenticated, IsOwner]
 
 
 class CommentaryViewSet(viewsets.ModelViewSet):
@@ -21,3 +29,7 @@ class CommentaryViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         article_obj = get_object_or_404(Article, pk=self.kwargs.get("pk"))
         serializer.save(author=self.request.user, article=article_obj)
+
+    def get_permissions(self):
+        if self.action in ('destroy', 'update', 'partial_update'):
+            self.permission_classes = [IsAuthenticated, IsOwner]
